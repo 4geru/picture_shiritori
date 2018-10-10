6aa1fd7e1807947a6bcdf34aa2c945bd0c77cf8d
Rp7rf <westhouse51@gmail.com>
2018-10-10 09:19:24 +0900

    save userId, groupId

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const config = {
   channelSecret: process.env.CHANNEL_SECRET,
 };
 
+const { Client } = require('pg');
+
+const pg_client = new Client({
+          connectionString: process.env.DATABASE_URL,
+          ssl: true,
+});
+
 // create LINE SDK client
 const client = new line.Client(config);
 
@@ -28,9 +35,16 @@ app.post('/callback', line.middleware(config), (req, res) => {
     });
 });
 
+pg_client.connect();
 // event handler
 function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
+  console.log(event.source.userId);
+	saveUserId(event.source.userId);
+ 	if(event.source.type == "group" || event.source.type == "room"){
+		const id = event.source.type == "group" ? event.source.groupId : event.source.roomId;
+		saveGroupId(id);
+	}
+	if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
@@ -47,3 +61,17 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
+
+const saveUserId = (userId) => {
+	const sql = `INSERT INTO users (user_id) VALUES('${userId}') RETURNING *`;
+	pg_client.query(sql, (err, res) => {
+		console.log(err ? `already inserted user_id ${userId}` : res.rows[0])
+	})
+}
+
+const saveGroupId = (groupId) => {
+	const sql = `INSERT INTO groups (group_id) VALUES('${groupId}') RETURNING *`;
+	pg_client.query(sql, (err, res) => {
+		console.log(err ? `already inesrted group_id ${groupId}` : res.rows[0])
+	})
+}
